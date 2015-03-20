60b2a3f1904be29701b74d55c93e6da4202e029e
runningnsy <904178767@qq.com>
2015-03-20 15:00:47 +0800

    add BBS

---
+++ src/JspToServlet/ShowArticleTree_jsp.java
@@ -0,0 +1,185 @@
+package JspToServlet;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.jsp.JspFactory;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.SkipPageException;
+
+public final class ShowArticleTree_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+
+String str = "";
+boolean login = false;
+private void tree(Connection conn, int id, int level) {
+	Statement stmt = null;
+	ResultSet rs = null;
+	String preStr = "";
+	for(int i=0; i<level; i++) {
+		preStr += "----";
+	}
+	try {
+		stmt = conn.createStatement();
+		String sql = "select * from article where pid = " + id;
+		rs = stmt.executeQuery(sql);
+		String strLogin = "";
+		
+		while(rs.next()) {
+			if(login) {
+				strLogin = "<td><a href='Delete.jsp?id=" + rs.getInt("id") + "&pid=" + rs.getInt("pid") + "'>É¾³ý</a>";
+			}
+			str += "<tr><td>" + rs.getInt("id") + "</td><td>" +
+			       preStr + "<a href='ShowArticleDetail.jsp?id=" + rs.getInt("id") + "'>" + 
+			       rs.getString("title") + "</a></td>" +
+			       strLogin +
+			       "</td></tr>";
+			if(rs.getInt("isleaf") != 0) {
+				tree(conn, rs.getInt("id"), level+1);
+			}
+		}
+	} catch (SQLException e) {
+		e.printStackTrace();
+	} finally {
+		try {
+			if(rs != null) {
+				rs.close();
+				rs = null;
+			}
+			if(stmt != null) {
+				stmt.close();
+				stmt = null;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List _jspx_dependants;
+
+  private javax.el.ExpressionFactory _el_expressionfactory;
+  private org.apache.AnnotationProcessor _jsp_annotationprocessor;
+
+  public Object getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspInit() {
+    _el_expressionfactory = _jspxFactory.getJspApplicationContext(getServletConfig().getServletContext()).getExpressionFactory();
+    _jsp_annotationprocessor = (org.apache.AnnotationProcessor) getServletConfig().getServletContext().getAttribute(org.apache.AnnotationProcessor.class.getName());
+  }
+
+  public void _jspDestroy() {
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+
+    try {
+      response.setContentType("text/html; charset=gbk");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+
+String admin = (String)session.getAttribute("admin");
+if(admin != null && admin.equals("true")) {
+	login = true;
+}
+
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+
+Class.forName("com.mysql.jdbc.Driver");
+String url = "jdbc:mysql://localhost/bbs?user=root&password=root";
+Connection conn = DriverManager.getConnection(url);
+
+Statement stmt = conn.createStatement();
+ResultSet rs = stmt.executeQuery("select * from article where pid = 0");
+String strLogin = "";
+while(rs.next()) {
+	if(login) {
+		strLogin = "<td><a href='Delete.jsp?id=" + rs.getInt("id") + "&pid=" + rs.getInt("pid") + "'>É¾³ý</a>";
+	}
+	str += "<tr><td>" + rs.getInt("id") + "</td><td>" +
+    	   "<a href='ShowArticleDetail.jsp?id=" + rs.getInt("id") + "'>" + 
+           rs.getString("title") + "</a></td>" +
+           strLogin +
+           "</td></tr>";
+   	if(rs.getInt("isleaf") != 0) {
+   		tree(conn, rs.getInt("id"), 1);
+   	}
+}
+rs.close();
+stmt.close();
+conn.close();
+
+      out.write("\r\n");
+      out.write("\n");
+      out.write("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n");
+      out.write("<html>\n");
+      out.write("<head>\n");
+      out.write("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=gbk\">\n");
+      out.write("<title>Insert title here</title>\n");
+      out.write("</head>\n");
+      out.write("<body>\r\n");
+      out.write("<a href=\"Post.jsp\">·¢±íÐÂÌû</a>\n");
+      out.write("<table border=\"1\">\r\n");
+      out.print( str );
+      out.write('\r');
+      out.write('\n');
+ 
+str = ""; 
+login = false;
+
+      out.write("\r\n");
+      out.write("</table>\r\n");
+      out.write("</body>\r\n");
+      out.write("\r\n");
+      out.write("</html>");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          try { out.clearBuffer(); } catch (java.io.IOException e) {}
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
